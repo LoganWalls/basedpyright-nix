8c840f57f8f69aa4a71433a00c3d43584c0aee5b
clamydo <clamydo@users.noreply.github.com>
2024-05-19 14:43:26 +0200

    update to 1.12.2

--- flake.nix
+++ flake.nix
@@ -15,11 +15,11 @@
         nodejs-bin = pkgs.callPackage ./nodejs-bin {inherit python3;};
         basedpyright = python3.pkgs.buildPythonPackage {
           pname = "basedpyright";
-          version = "1.6.1";
+          version = "1.12.2";
           format = "wheel";
           src = pkgs.fetchurl {
-            url = "https://files.pythonhosted.org/packages/35/64/a0c23a5cb0fee6f860feb221027ff95c7b00d41992438a41f3a816c99794/basedpyright-1.6.1-py3-none-any.whl";
-            hash = "sha256-NUOeJY8wbJcWotohCi6BCo49GU8+JIOA4tJsuvEtDss=";
+            url = "https://files.pythonhosted.org/packages/0f/fa/9a358a171581c3452cd933f08a3ad7501acbe0ab4ec1acb894c7bb08b2fc/basedpyright-1.12.2-py3-none-any.whl";
+            hash = "sha256-l/O/Juys9hHZMYMCKtoLu+8hhDOMsZPsRwJyPbOdTFE=";
           };
           propagatedBuildInputs = [nodejs-bin];
         };
